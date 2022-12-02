b7c817a2b096ccc3024d8a313b6069b3e94a6547
ThatMadMf <maxdarksiders@gmail.com>
2022-12-02 15:53:14 +0200

    Implement deleting user

--- src/store/user.module.js
+++ src/store/user.module.js
@@ -3,6 +3,8 @@ import router from "@/router";
 
 export const GET_USERS = 'GET_USERS';
 export const GET_USER = 'GET_USER';
+export const CREATE_USER = 'CREATE_USER';
+export const DELETE_USER = 'DELETE_USER';
 export const LOGIN = 'LOGIN';
 
 export const SET_USERS = 'SET_USERS';
@@ -30,7 +32,8 @@ const userModule = {
                     context.commit(SET_USER, response.data);
                     context.commit(SET_TOKEN, token);
                 });
-        }, [LOGIN](context, user) {
+        },
+        [LOGIN](context, user) {
             ApiService.post('/login', user)
                 .then((response) => {
                     setUserData(context, response.data);
@@ -39,6 +42,24 @@ const userModule = {
                 .catch(({response}) => {
                     console.error(response.data.detail);
                 });
+        },
+        [CREATE_USER](context, user) {
+            ApiService.post('/users', user)
+                .then((response) => {
+                    context.commit(SET_USERS, [...context.getters.users, response.data]);
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        },
+        [DELETE_USER](context, id) {
+            ApiService.delete(`/users/${id}`)
+                .then(() => {
+                    context.commit(SET_USERS, context.getters.users.filter((u) => u.id !== id));
+                })
+                .catch((err) => {
+                    console.error(err);
+                })
         },
     }, mutations: {
         [SET_USERS](state, users) {
@@ -49,14 +70,15 @@ const userModule = {
         },
         [SET_TOKEN](state, token) {
             state.token = token;
-        }
+        },
     }, getters: {
         users(state) {
             return state.users;
         },
         user(state) {
             return state.user;
-        }, token(state) {
+        },
+        token(state) {
             return state.token;
         },
     }
--- src/views/TheUsersPage.vue
+++ src/views/TheUsersPage.vue
@@ -1,13 +1,42 @@
 <template>
   <div class="sessions-wrapper">
     <h2>Users</h2>
-    <a-table :columns="columns" :dataSource="users"/>
+    <div class="controls-wrapper">
+      <a-button
+          type="primary"
+          @click="showCreateUserModal"
+      >
+        Create user
+      </a-button>
+    </div>
+    <a-table :columns="columns" :dataSource="users">
+      <span slot="action" slot-scope="text, record">
+        <a-divider type="vertical"/>
+        <a @click="deleteUser(record.id)">Delete user</a>
+      </span>
+    </a-table>
+    <a-modal
+        :visible="createUserVisible"
+        @cancel="() => this.createUserVisible = false"
+        @ok="createUser"
+    >
+      <h3>Fill user fields</h3>
+      <a-input v-model="username" class="modal-input" placeholder="Username"/>
+      <a-input v-model="firstName" class="modal-input" placeholder="First name"/>
+      <a-input v-model="lastName" class="modal-input" placeholder="Last name"/>
+    </a-modal>
+    <a-modal
+        :visible="addFaceEncodingVisible"
+        @cancel="() => this.addFaceEncodingVisible = false"
+        @ok="() => this.addFaceEncodingVisible = false"
+    >
+    </a-modal>
   </div>
 </template>
 
 <script>
 import {mapGetters} from "vuex";
-import {GET_USERS} from "@/store/user.module";
+import {CREATE_USER, DELETE_USER, GET_USERS} from "@/store/user.module";
 
 export default {
   name: "TheUsersPage",
@@ -18,6 +47,8 @@ export default {
   },
   data() {
     return {
+      createUserVisible: false,
+      addFaceEncodingVisible: false,
       columns: [
         {
           title: 'ID',
@@ -49,8 +80,33 @@ export default {
           dataIndex: 'dateJoined',
           key: 'dateJoined',
         },
+        {
+          title: 'Action',
+          key: 'action',
+          scopedSlots: {customRender: 'action'},
+        },
       ],
+      username: '',
+      firstName: '',
+      lastName: '',
     };
+  },
+  methods: {
+    showCreateUserModal() {
+      this.createUserVisible = true;
+    },
+    createUser() {
+      this.$store.dispatch(CREATE_USER, {
+        username: this.username,
+        firstName: this.firstName,
+        lastName: this.lastName,
+      });
+
+      this.createUserVisible = false;
+    },
+    deleteUser(id) {
+      this.$store.dispatch(DELETE_USER, id);
+    }
   },
   mounted() {
     this.$store.dispatch(GET_USERS);
@@ -59,5 +115,13 @@ export default {
 </script>
 
 <style scoped>
+.controls-wrapper {
+  display: flex;
+  flex-direction: row-reverse;
+  padding: 1rem 3rem;
+}
 
+.modal-input {
+  margin: 0.5rem 0;
+}
 </style>
